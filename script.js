75dd3b54f4bb274358063744d7310c4f90fc3607
Maxie Wright <maxiewright@gmail.com>
2024-02-25 10:29:04 -0400

    add ui to rock-paper-scissors

--- script.js
+++ script.js
@@ -4,33 +4,86 @@
 const choices = ['rock', 'paper', 'scissors'];
 let computerScore = 0;
 let playerScore = 0;
+let playerSelection;
+let message = '';
+let roundPlayed = 0;
+let roundsToPlay = 5;
 
+let container = document.querySelector('#container');
+let messageContainer = document.querySelector('#message');
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
+let playerScoreBoard = document.querySelector('#player-score');
+playerScoreBoard.textContent = 0;
+
+let computerScoreBoard = document.querySelector('#computer-score');
+computerScoreBoard.textContent = 0;
+
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', () =>  playGame('rock'));
+
+let paper = document.querySelector('#paper')
+paper.addEventListener('click', () => playGame('paper'));
+
+let scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => playGame('scissors'));
+
+let resetBtn = document.querySelector('#reset');
+
+resetBtn.addEventListener('click', () => reset())
+
+function reset(){
+    computerScore = 0;
+    playerScore = 0;
+    roundPlayed = 0;
+    messageContainer.textContent = '';
+    playerScoreBoard.textContent = 0;
+    computerScoreBoard.textContent = 0;
 }
 
+function playGame(playerSelection){
+
+    if(roundPlayed <= roundsToPlay){
+        playRound(playerSelection) 
+
+        messageContainer.textContent = message
+
+        playerScoreBoard.textContent = playerScore;
+
+        computerScoreBoard.textContent = computerScore;
+
+        return roundPlayed++;
+    }
+
+    return determineWinner();
+}
+
+function determineWinner(){
+    if (playerScore === computerScore) {
+        return messageContainer.textContent = `The game is tied Computer: ${computerScore} vs Player: ${playerScore}`;
+     }
+ 
+     if (playerScore > computerScore) {
+        return messageContainer.textContent = `You win with a score of ${playerScore} vs ${computerScore}`;
+     }
+ 
+     return messageContainer.textContent = `Computer wins with a score of ${computerScore} vs ${playerScore}`;
+}
 
 function playRound(playerSelection) {
 
     let computerSelection = getComputerChoice();
     playerSelection = playerSelection.toLowerCase();
-    
-
-    while (!choices.includes(playerSelection)) {
-        playerSelection = prompt('Wrong selection, Please enter either Rock, Paper or Scissors')
-    }
 
     if (computerSelection === playerSelection) {
-        playerSelection = prompt('Its a tie! Please select again');
-        return playRound(playerSelection);
+        message = 'Its a tie! Please select again';   
+        return;
     }
 
     if (playerSelection === "rock" && computerSelection === "scissors"
         || playerSelection === 'paper' && computerSelection === 'rock'
         || playerSelection === 'scissors' && computerSelection === 'paper') {
 
-        console.log(`You Win, ${playerSelection} beats ${computerSelection}`)
+        message = `You win his round, ${playerSelection} beats ${computerSelection}`;
 
         return playerScore++;
     }
@@ -39,26 +92,12 @@ function playRound(playerSelection) {
         || computerSelection === 'paper' && playerSelection === 'rock'
         || computerSelection === 'scissors' && playerSelection === 'paper') {
 
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+        message = `You lose this round, ${computerSelection} beats ${playerSelection}`;
 
         return computerScore++;
     }
 }
 
-function game(rounds = 5) {
-
-    for (let i = 0; i < rounds; i++) {
-        let playerSelection = prompt("Enter Selection")
-        playRound(playerSelection)
-    }
-
-    if (playerScore > computerScore) {
-        return `Player wins with a score of ${playerScore}`
-    }
-
-    return `Computer wins with a score of ${computerScore}`;
-
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
 }
-
-
-console.log(game());
